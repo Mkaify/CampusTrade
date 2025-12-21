afc6ec19757f00e84bd688d1ad386bb7c236d9c6
Mkaify <kaifurrehman46@gmail.com>
2025-12-21 12:43:45 +0500

    save critical files

--- lib/services/cloudinary_service.dart
+++ lib/services/cloudinary_service.dart
@@ -1,13 +1,21 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import this
 
 class CloudinaryService {
   // Replace with YOUR values from Phase 1
-  final String cloudName = "dtksqei0m";
-  final String uploadPreset = "campus_upload"; // The unsigned preset name
+  // Read from .env
+  final String? cloudName = dotenv.env['CLOUDINARY_CLOUD_NAME'];
+  final String? uploadPreset = dotenv.env['CLOUDINARY_UPLOAD_PRESET'];
 
   Future<String?> uploadImage(File imageFile) async {
+    // Safety check
+    if (cloudName == null || uploadPreset == null) {
+      print("❌ Cloudinary config missing in .env file");
+      return null;
+    }
+
     try {
       var uri = Uri.parse("https://api.cloudinary.com/v1_1/$cloudName/upload");
 
@@ -21,7 +29,7 @@ class CloudinaryService {
       request.files.add(multipartFile);
 
       // Add the upload preset (Security key for unsigned uploads)
-      request.fields['upload_preset'] = uploadPreset;
+      request.fields['upload_preset'] = uploadPreset!;
 
       // Send request
       var response = await request.send();
